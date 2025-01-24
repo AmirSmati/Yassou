65fbeca9110670e6ad706e4b4684df2563721ba8
AmirSmati <156704728+AmirSmati@users.noreply.github.com>
2025-01-24 15:17:19 +0100

    fix conent

--- src/app/components/Card.jsx
+++ src/app/components/Card.jsx
@@ -5,7 +5,7 @@ export default function Card(props) {
     <div className="card bg-[#bc2b48] w-80 shadow-xl">
       <div className="card-body">
         <h2 className="card-title text-white">{props.typo}</h2>
-        <p className="text-white">{props.context}</p>
+        <p className="text-white">{props.content}</p>
         <div className="card-actions justify-end">
           <button
             onClick={props.onDelete} // Call the onDelete function
